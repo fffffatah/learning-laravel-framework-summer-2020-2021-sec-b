0b245cda05ea5652fbcf5ebc6ad3534237ca1280
Muhammad Fatah <ab.fatahmn@gmail.com>
2021-06-09 12:31:18 +0600

    lab 2 task

---
+++ LoginSignupLaravel/resources/views/user/create.blade.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Signup</title>
+</head>
+<body>
+    <form method="post">
+	<table>
+        <tr>
+			<td>Email</td>
+			<td><input type="text" name="email"></td>
+		</tr>
+        <tr>
+			<td>Full Name</td>
+			<td><input type="text" name="fullname"></td>
+		</tr>
+		<tr>
+			<td>User Name</td>
+			<td><input type="text" name="uname"></td>
+		</tr>
+		<tr>
+			<td>Password</td>
+			<td><input type="password" name="pass"></td>
+		</tr>
+        <tr>
+			<td>Confirm Password</td>
+			<td><input type="password" name="confirmpass"></td>
+		</tr>
+		<tr>
+			<td></td>
+			<td><input type="submit" name="create" value="Create"></td>
+		</tr>
+	</table>
+	</form>
+</body>
+</html>
